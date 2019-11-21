ca96544dc4f3797d0694cb223c0db595775fc7e5
acoltelli <acoltelli@protonmail.com>
2019-11-21 15:54:57 -0800

    remove unecessary script file

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -99,3 +99,17 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector("#user_message").focus();
     }
 });
+
+// sidebar collapse
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#show-sidebar-button').onclick = () => {
+        document.querySelector('#sidebar').classList.toggle('view-sidebar');
+    };
+    let msg = document.getElementById("user_message");
+    msg.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("send_message").click();
+        }
+    });
+});
